d1a4c787530a45b06a81e28ec4140f65fb9da9a0
liamalexander <liamalexander7@hotmail.co.uk>
2020-07-27 11:40:05 +0100

    added about page

--- backend/index.js
+++ backend/index.js
@@ -9,4 +9,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../frontend/index.html'));
 });
 
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../frontend/about.html'));
+});
+
 app.listen(3000);
