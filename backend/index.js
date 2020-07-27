55bd2dc2c36cdf0f51ce4ee795ffe8537c1f363a
liamalexander <liamalexander7@hotmail.co.uk>
2020-07-27 11:09:03 +0100

    added route info static files

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-// viewed at http://localhost:8080
+app.use(express.static(__dirname + "/../frontend"))
+
+// viewed at http://localhost:3000
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../frontend/index.html'));
 });
