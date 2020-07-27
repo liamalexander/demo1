d73dcab861660039dd8a6647dc4ce3735ce7900f
liamalexander <liamalexander7@hotmail.co.uk>
2020-07-27 14:40:30 +0100

    setup newsletter api

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,27 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017', {useNewUrlParser: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("connected db");
+});
+
+const newsletterSchema = new mongoose.Schema({
+    email: String
+});
+
+const Newsletter = mongoose.model('Newsletter', newsletterSchema);
+
+const router = express.Router();
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + "/../frontend"))
 
@@ -15,6 +36,25 @@ app.get('/about', function(req, res) {
 
 app.get('/contact', function(req, res) {
     res.sendFile(path.join(__dirname + '/../frontend/contact.html'));
+});
+
+app.post('/api/newsletter',(request, response) => {
+    const { email } = request.body;
+    
+    if (!email) {
+        response.status(400).json({
+            message: "email is required!"
+        })
+    }
+
+    const newRecord = new Newsletter({ email });
+
+    newRecord.save(function (err, fluffy) {
+        if (err) return console.error(err);
+        response.json({
+            "success": true
+        })
+    });
 });
 
 app.listen(3000);
