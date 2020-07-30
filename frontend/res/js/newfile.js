ce1cb474a58ca95b6407ccfefbd049fc0acbc66f
liamalexander <liamalexander7@hotmail.co.uk>
2020-07-30 11:34:07 +0100

    remove button new file

--- frontend/res/js/newfile.js
+++ frontend/res/js/newfile.js
@@ -1,18 +1,30 @@
 let counter = 0;
 
-window.addEventListener("load", () => {
+window.addEventListener("load", () => { 
     const counterOutput = document.getElementById("counter");
     
     function outputCounter() {
         counterOutput.innerHTML = counter;
     }
 
-    function addCounter() {
+    function addCounter(){
         counter = counter + 1;
         outputCounter();
     }
 
+    function removeCounter(){
+        if (counter > 0);
+        counter = counter - 1;
+        outputCounter();
+    }
+
     const buttonAdd = document.getElementById("buttonAdd");
-    buttonAdd.addEventListener("click", () => {
+    buttonAdd.addEventListener("click", () => { 
         addCounter();
-    }});
+    }
+    ,)})
+
+    const buttonRemove = document.getElementById("buttonRemove");
+    buttonRemove.addEventListener("click", () => { 
+        removeCounter();
+    });
