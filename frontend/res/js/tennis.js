ab0f6def1c3af066678d1178a2b60275e1b37294
liamalexander <liamalexander7@hotmail.co.uk>
2021-01-08 13:12:50 +0000

    add full page tabs to rivalries

--- frontend/res/js/tennis.js
+++ frontend/res/js/tennis.js
@@ -9,6 +9,24 @@ for (i = 0; i < accordion.length; i++) {
     })
 }
 
+let tabButtons = document.querySelectorAll(".tabContainer .buttonContainer button");
+let tabPanels = document.querySelectorAll(".tabContainer .tabPanel");
+
+function showPanel(panelIndex, colorCode) {
+    tabButtons.forEach(function(node) {
+        node.style.backgroundColor= "";
+        node.style.color = "";
+    });
+    tabButtons[panelIndex].style.backgroundColor = colorCode;
+    tabButtons[panelIndex].style.color = "white";
+    tabPanels.forEach(function(node) {
+        node.style.display = "none";
+    });
+    tabPanels[panelIndex].style.display = "block";
+    tabPanels[panelIndex].style.backgroundColor = colorCode;
+}
+showPanel(0, '#03befc');
+
 function changeColour() {
     const wrongA = document.getElementById("ans1a");
     wrongA.style.backgroundColor = "red";
